131debb2e1568e7acc4b90cddc0d609412fe214b
Pedro Gonzalez <pdro74@hotmail.com>
2019-07-30 22:14:34 +0200

    create components list, create and detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostCreateComponent } from './post-create/post-create.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostListComponent, pathMatch: 'full' },
+  { path: 'detail/:postId', component: PostDetailComponent },
+  { path: 'new', component: PostCreateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostCreateComponent } from './post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostDetailComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
